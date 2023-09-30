ce12eacfc3c5fca78ab9d7dc569ee5d6978e133a
fahricandurucan <fahricandurucan@icloud.com>
2023-09-30 14:57:24 +0300

    pagination is set and small changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:social_media_app_fl/services/post_api.dart';
-import 'package:social_media_app_fl/utils/const.dart';
+import 'package:flutterflow_paginate_firestore/paginate_firestore.dart';
+import 'package:social_media_app_fl/models/post.dart';
 import 'package:social_media_app_fl/widgets/animated_text_widget.dart';
 import 'package:social_media_app_fl/widgets/post_widget.dart';
 
@@ -22,53 +23,112 @@ class HomePage extends StatelessWidget {
           text: "SoSocial",
         ),
       ),
-      body: StreamBuilder(
-        stream: PostApi.getAllPosts(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            var postList = snapshot.data;
-            postList!.addAll(Const.dummyPosts);
-            print("snapshot data = ${postList.length}");
-            return postList.isNotEmpty
-                ? ListView.builder(
-                    itemCount: postList.length + 1, // 1 yatay listview eklendi
-                    itemBuilder: (context, index) {
-                      if (index == 0) {
-                        return SizedBox(
-                          height: 100,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: 10,
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding: const EdgeInsets.all(12.0),
-                                child: Container(
-                                  height: 100,
-                                  width: 100,
-                                  decoration: const BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: Colors.greenAccent,
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        );
-                      } else {
-                        final post = postList[index - 1];
-                        return PostWidget(
-                          post: post,
-                        );
-                      }
-                    },
-                  )
-                : const Center(
-                    child: Text("error"),
-                  );
-          }
-          return const Center(child: CircularProgressIndicator());
-        },
+      body: Column(
+        children: [
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.greenAccent,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: PaginateFirestore(
+              query: FirebaseFirestore.instance.collection("allPost"),
+              itemBuilderType: PaginateBuilderType.listView,
+              itemsPerPage: 3,
+              isLive: true,
+              initialLoader: const Center(child: CircularProgressIndicator.adaptive()),
+              onEmpty: const Center(
+                child: Text("Empty Data"),
+              ),
+              onError: (e) => const Center(
+                child: Text("error loading data"),
+              ),
+              bottomLoader: const Center(child: CircularProgressIndicator.adaptive()),
+              itemBuilder: (context, snapshot, index) {
+                final Map<String, dynamic> json = snapshot[index].data() as Map<String, dynamic>;
+
+                final Timestamp date = json['date'];
+                final String description = json['description'];
+                final String image = json['image'];
+                final String title = json['title'];
+                final String userName = json['userName'];
+
+                Post newPost = Post(
+                  userName: userName,
+                  title: title,
+                  description: description,
+                  image: image,
+                  date: date,
+                );
+
+                return PostWidget(post: newPost);
+              },
+            ),
+          ),
+        ],
       ),
+      // body: StreamBuilder(
+      //   stream: PostApi.getAllPosts(),
+      //   builder: (context, snapshot) {
+      //     if (snapshot.hasData) {
+      //       var postList = snapshot.data;
+      //       postList!.addAll(Const.dummyPosts);
+      //       print("snapshot data = ${postList.length}");
+      //       return postList.isNotEmpty
+      //           ? ListView.builder(
+      //               itemCount: postList.length + 1, // 1 yatay listview eklendi
+      //               itemBuilder: (context, index) {
+      //                 if (index == 0) {
+      //                   return SizedBox(
+      //                     height: 100,
+      //                     child: ListView.builder(
+      //                       scrollDirection: Axis.horizontal,
+      //                       itemCount: 10,
+      //                       itemBuilder: (context, index) {
+      //                         return Padding(
+      //                           padding: const EdgeInsets.all(12.0),
+      //                           child: Container(
+      //                             height: 100,
+      //                             width: 100,
+      //                             decoration: const BoxDecoration(
+      //                               shape: BoxShape.circle,
+      //                               color: Colors.greenAccent,
+      //                             ),
+      //                           ),
+      //                         );
+      //                       },
+      //                     ),
+      //                   );
+      //                 } else {
+      //                   final post = postList[index - 1];
+      //                   return PostWidget(
+      //                     post: post,
+      //                   );
+      //                 }
+      //               },
+      //             )
+      //           : const Center(
+      //               child: Text("error"),
+      //             );
+      //     }
+      //     return const Center(child: CircularProgressIndicator());
+      //   },
+      // ),
     );
   }
 }
